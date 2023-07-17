6b70fca961dea0b191f9173193bf4e9a038d171c
Vasu Vikram <vvikram@andrew.cmu.edu>
2023-07-17 16:44:06 -0400

    added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,8 +17,14 @@ function Footer() {
         </div>
         <div className="footerRight">
 				<footer>
+        <p>
 					Developed by <Link to="https://vasumv.github.io/">Vasu Vikram</Link> and the&nbsp;
           <Link to="https://cmu-pasta.github.io/">PASTA Lab</Link> @ Carnegie Mellon University.
+        </p>
+        <p>
+					Check out our <Link to="https://arxiv.org/abs/2307.04346">paper</Link> for more info! If you have any feedback or find any issues, please
+          report them <Link to="https://github.com/cmu-pasta/proptest.ai/issues">here</Link>.
+        </p>
 				</footer>
         </div>
 			</div>
